9d21cb7c5db4ed81b05314a80f2f50a725829dcd
Jeffrey <jeyao474@gmail.com>
2022-10-19 21:38:34 -0700

    added damage number indicators

--- src/Enemies/Shared/Enemy.gd
+++ src/Enemies/Shared/Enemy.gd
@@ -12,18 +12,28 @@ var alive = true
 var nav_path = ""
 var NAV = null
 
+var damagetext = preload("res://UI/DamageFloatingText.tscn")
+
 onready var global_pos = self.global_transform.origin
 
 func _ready():
 	health = max_health
 	#velocity = Vector3(1, velocity.y, 1)
 #func _physics_process(delta):
-	#self.global_position += velocity
+	#self.global_position += velocity\
 	
 func hurt_enemy(damage):
+	print("enemy position: ", global_pos)
 	health -= damage
+	var text = damagetext.instance()
+	add_child(text)
+	text.set_values(damage, global_pos)
+	print("Enemy takes: ", damage)
 	if health <= 0:
 		kill_enemy()
+		
+func spawn_damage_num(damage):
+	pass
 
 func kill_enemy():
 	
---
+++ src/UI/DamageFloatingText.gd
@@ -0,0 +1,20 @@
+extends Spatial
+
+onready var LABEL = $Label
+onready var AP = $AnimationPlayer
+onready var TWEEN = $Tween
+
+# Called when the node enters the scene tree for the first time.
+func ready():
+	pass
+	
+func set_values(value, start_pos):
+#	self.transform.origin = start_pos
+	LABEL.text = value as String
+	self.global_transform.origin = start_pos
+	self.global_transform.origin.y += 1
+	var end_pos = Vector3(rand_range(-2, 2), rand_range(0, 2), rand_range(-2, 2)) + translation
+	
+	var tween_length = AP.get_animation("ShowDamage").length
+	TWEEN.interpolate_property(self, "translation", translation, end_pos, tween_length, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+	TWEEN.start()
