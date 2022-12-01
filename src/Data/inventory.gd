dcda99761644f53fcbdafa2b25343915e7dcb69e
AbowlofStew <saxup@ucsc.edu>
2022-11-30 16:21:41 -0800

    finished with inventory and save need to hook into main and items to test
    I need help on where to put into main

--- src/Data/inventory.gd
+++ src/Data/inventory.gd
@@ -2,26 +2,64 @@ extends Node
 
 var path = "res://Data/partyinventory.json"
 
+var default_inventory_data = {
+	"player1":{
+		"mod" :"null"
+	},
+	"player2":{
+		"mod":"null"
+	},
+	"player3":{
+		"mod":"null"
+	},
+	"party":{
+		"items" : []
+	},
+}
 var inventory_data = {
 	"player1":{
 		"mod" :"null"
-	}
-	"player2"{
+	},
+	"player2":{
 		"mod":"null"
-	}
-	"player3"{
+	},
+	"player3":{
 		"mod":"null"
-	}
-	"party"{
+	},
+	"party":{
 		"items" : []
-	}
+	},
 }
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var file = File.new()
-	file.open("res://Data/partyinventory.json", File.READ)
-	var json = JSON.parse(file.get_as_text())
-	file.close()
-	inventory = json.result
+	load_game()
+	update_game_data()
 	
+func load_game():
+	var file = File.new()
+
+	if not file.file_exists(path):
+		reset_data()
+		return
+		
+	file.open(path, file.READ)
+	inventory_data = parse_json(file.get_as_text())
+	file.close()
+
+func save_data():
+	var file
+	file = File.new()
+	file.open(path, File.WRITE)
+	file.store_line(to_json(inventory_data))
+	file.close()
+
+func reset_data():
+	var file
+	file = File.new()
+	file.open(path, File.WRITE)
+	file.store_line(to_json(default_inventory_data))
+	file.close()
+
+func update_game_data():
+	pass
